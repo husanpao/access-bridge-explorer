49d37afaa32d0c426507c2b43b91ae08ef030203
huhusen <ndk@live.com>
2025-08-04 09:02:18 +0800

    feat: 添加属性列表右键菜单及复制功能
    新增属性列表右键菜单，支持“复制属性值”和“复制所有属性”功能，并添加快捷键 Ctrl+C 直接复制选中属性值到剪贴板，提升用户操作便捷性。

--- src/AccessBridgeExplorer/PropertyListView.cs
+++ src/AccessBridgeExplorer/PropertyListView.cs
@@ -16,6 +16,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.Text;
 using WindowsAccessBridgeInterop;
 using AccessBridgeExplorer.Utils;
 
@@ -31,16 +32,32 @@ namespace AccessBridgeExplorer {
     private readonly TreeListView<PropertyNode> _treeListView;
     private readonly HashSet<PropertyGroup> _knownGroups = new HashSet<PropertyGroup>();
     private PropertyList _currentPropertyList;
+    private readonly ListView _listView;
+    private readonly ContextMenuStrip _contextMenu;
 
     /// <summary>
     /// Setup event handlers and image list. Can be called in the Forms
     /// constructor, just after <code>InitializeComponent</code>.
     /// </summary>
     public PropertyListView(ListView listView, ImageList stateImageList) {
+      _listView = listView;
       _treeListView = new TreeListView<PropertyNode>(listView, stateImageList);
       _treeListView.NodeVisibilityChanged += TreeListViewOnNodeVisibilityChanged;
       listView.SelectedIndexChanged += ListOnSelectedIndexChanged;
       listView.GotFocus += ListViewOnGotFocus;
+      listView.KeyDown += ListViewOnKeyDown;
+      
+      // 创建右键菜单
+      _contextMenu = new ContextMenuStrip();
+      var copyMenuItem = new ToolStripMenuItem("复制属性值");
+      copyMenuItem.Click += CopyMenuItem_Click;
+      _contextMenu.Items.Add(copyMenuItem);
+      
+      var copyAllMenuItem = new ToolStripMenuItem("复制所有属性");
+      copyAllMenuItem.Click += CopyAllMenuItem_Click;
+      _contextMenu.Items.Add(copyAllMenuItem);
+      
+      listView.ContextMenuStrip = _contextMenu;
     }
 
     public event EventHandler<AccessibleRectInfoSelectedEventArgs> AccessibleRectInfoSelected;
@@ -76,6 +93,13 @@ namespace AccessBridgeExplorer {
 
     private void ListViewOnGotFocus(object sender, EventArgs eventArgs) {
       ListOnSelectedIndexChanged(sender, eventArgs);
+    }
+
+    private void ListViewOnKeyDown(object sender, KeyEventArgs e) {
+      if (e.Control && e.KeyCode == Keys.C) {
+        CopyMenuItem_Click(sender, EventArgs.Empty);
+        e.Handled = true;
+      }
     }
 
     private void ListOnSelectedIndexChanged(object sender, EventArgs eventArgs) {
@@ -139,6 +163,43 @@ namespace AccessBridgeExplorer {
     protected virtual void OnError(PropertyGroupErrorEventArgs e) {
       var handler = Error;
       if (handler != null) handler(this, e);
+    }
+
+    /// <summary>
+    /// 复制选中属性的值到剪贴板
+    /// </summary>
+    private void CopyMenuItem_Click(object sender, EventArgs e) {
+      if (_listView.SelectedItems.Count == 0)
+        return;
+
+      var selectedItem = _listView.SelectedItems[0];
+      if (selectedItem.SubItems.Count > 1) {
+        var value = selectedItem.SubItems[1].Text;
+        if (!string.IsNullOrEmpty(value)) {
+          Clipboard.SetText(value);
+        }
+      }
+    }
+
+    /// <summary>
+    /// 复制所有属性到剪贴板
+    /// </summary>
+    private void CopyAllMenuItem_Click(object sender, EventArgs e) {
+      if (_listView.Items.Count == 0)
+        return;
+
+      var sb = new StringBuilder();
+      foreach (ListViewItem item in _listView.Items) {
+        if (item.SubItems.Count > 1) {
+          var property = item.Text;
+          var value = item.SubItems[1].Text;
+          sb.AppendLine(property + ": " + value);
+        }
+      }
+
+      if (sb.Length > 0) {
+        Clipboard.SetText(sb.ToString());
+      }
     }
   }
 }
